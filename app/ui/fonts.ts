79676d0941bfc9865bd843484a3db38309244078
Luca Calvetti <calvetti.luca@gmail.com>
2025-04-13 03:20:29 +0200

    First Commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Lato, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const lato = Lato({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
